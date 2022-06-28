3cbe97123aca38169ddd283cc42862072b02e4f2
rolando22 <rolando2207@gmail.com>
2022-06-28 19:29:52 -0300

    Setup del proyecto

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/global.scss";
+
+function App () {
+    return (
+        <h1>Hola Mundo!, Bienvenido al Curso Práctico de React</h1>
+    );
+}
+
+export { App };
