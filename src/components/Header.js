130c7ff7c216547768d7f3855a14edd46cae4154
rolando22 <rolando2207@gmail.com>
2022-06-29 11:34:39 -0300

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles/Header.scss";
+
+function Header () {
+    return (
+        <nav>
+            <img src="./icons/Platzi_YardSale_Icons/icon_menu.svg" alt="menu" className="menu" />
+            <div className="navbar-left">
+                <img src="./logos/Platzi_YardSale_Logos/logo_yard_sale.svg" alt="logo-menu" className="logo-menu" />
+                <ul>
+                    <li><a href="/">All</a></li>
+                    <li><a href="/">Clothes</a></li>
+                    <li><a href="/">Electronics</a></li>
+                    <li><a href="/">Furnitures</a></li>
+                    <li><a href="/">Toys</a></li>
+                    <li><a href="/">Others</a></li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <ul>
+                    <li className="navbar-email">platzi@example.com</li>
+                    <li className="navbar-shopping-cart">
+                        <img src="./icons/Platzi_YardSale_Icons/icon_shopping_cart.svg" alt="shopping-cart" />
+                        <div>2</div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export { Header };
