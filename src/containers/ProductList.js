9cde3a9282e838a5686161c473b9b477508078df
rolando22 <rolando2207@gmail.com>
2022-07-02 20:14:41 -0300

    Realizando petición a la API para traer los productos

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ProductItem } from "@components/ProductItem";
 import "@styles/ProductList.scss";
+import axios from "axios";
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 function ProductList () {
+    const [products, setProducts] =  useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await axios.get(API);
+            setProducts(response.data);
+        };
+        getData();
+        console.log(products);
+    }, []);
+
     return (
         <section className="main-container">
             <div className="cards-container">
-                <ProductItem />
+                {products.map(product => (
+                    <ProductItem />
+                ))}
             </div>
         </section>
     );
