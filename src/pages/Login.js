bd8956e18eaf3b6cf082c7677e91779c4a4fffa4
rolando22 <rolando2207@gmail.com>
2022-07-02 21:47:19 -0300

    Aplicando useRef para el form del Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
 import "@styles/Login.scss";
 
 import logoYardSale from "@logos/logo_yard_sale.svg";
 
 function Login () {
+    const form = useRef(null);
+
+    const handleSubmit = () => {
+        const formData = new FormData(form.current);
+        const data = {
+            userName: formData.get('email'),
+            password: formData.get('password'),
+        };
+    };
+
     return (
         <div className="login">
             <div className="form-container">
                 <img src={logoYardSale} alt="logo" className="logo" />
-                <form action="/" className="form">
-                    <label htmlFor="email" className="label">Email address</label>
-                    <input type="text" id="email" placeholder="platzi@example.com" className="input input-email" />
-                    <label htmlFor="password" className="label">Password</label>
-                    <input type="password" id="password" placeholder="*********" className="input input-password" />
-                    <input type="submit" value="Log in" className="primary-button login-button" />
+                <form action="/" className="form" ref={form}>
+                    <label
+                        htmlFor="email"
+                        className="label"
+                    >
+                        Email address
+                    </label>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="platzi@example.com"
+                        className="input input-email"
+                    />
+                    <label
+                        htmlFor="password"
+                        className="label"
+                    >
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="*********"
+                        className="input input-password"
+                    />
+                    <button
+                        type="button"
+                        className="primary-button login-button"
+                        onClick={handleSubmit}
+                    >
+                        Log in
+                    </button>
                     <a href="/">Forgot my password</a>
                 </form>
                 <button className="secondary-button sigup-button">Sign up</button>
